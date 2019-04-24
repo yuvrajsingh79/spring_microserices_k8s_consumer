a3a83bd0ed2ec30fba239607ff7aaf4b7377dd54
yuvrajsingh79 <singhyuvraj79@gmail.com>
2019-04-24 13:18:09 +0530

    initial commit

---
+++ consume-service/src/main/java/com/scorp/consumeservice/ConsumeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.scorp.consumeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeServiceApplication.class, args);
+	}
+
+}
